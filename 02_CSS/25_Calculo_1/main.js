b9f317ad0f990c268a770baa3e6e98b239d22a7e
samupapati <samuelpapati@gmail.com>
2023-05-17 21:43:16 -0300

    Estilizacao/CSS

---
+++ 02_CSS/25_Calculo_1/main.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt('Informe o primeiro número: '));
+let numero2 = Number(prompt('Informe o segundo número: '));
+let soma = numero1 + numero2;
+
+
+document.getElementById('resultado').innerHTML = soma;
+
